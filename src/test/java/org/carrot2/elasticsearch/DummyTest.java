aa8bef8878da3a651a9761caf38142ebf5548960
Dawid Weiss <dawid.weiss@carrotsearch.com>
2017-01-19 11:44:19 +0100

    Removed dummy unit test.

--- src/test/java/org/carrot2/elasticsearch/DummyTest.java
+++
@@ -1,7 +0,0 @@
-package org.carrot2.elasticsearch;
-
-import com.carrotsearch.randomizedtesting.RandomizedTest;
-
-public class DummyTest extends RandomizedTest {
-    public void testSuccess() {}
-}
